acd212f55d8f8472e06ee51694000a0bee9baee7
Ninjapouet <julien.blond@gmail.com>
2021-03-11 10:47:45 +0000

    Deploying to gh-pages from  @ 91cd4b5b468f0421009b89c38a60a542f1f2c396 🚀

---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'master (91cd4b5) (2021/03/11 10:47)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
